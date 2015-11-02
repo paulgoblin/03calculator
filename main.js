f60eb2db354c1b9bde8140d54ee017d9813de065
Michael Richter <michaelrichter@Michaels-MacBook-Pro-2.local>
2015-11-01 22:06:07 -0800

    add complete opration to all buttons

--- main.js
+++ main.js
@@ -1,179 +1,149 @@
-(function app() {
-  'use strict';
+'use strict';
 
-  let documentReady = () => {
-    
-    let $disp = $('#display');
-    let operatorDict = {add:1, subt:2, mult:3, divi:4};
-    //operation is comprized of an [operator, operand]
-    let oldDisp = 0;
-    let operator = 0;
-    let overwrite = false;
+let $disp = $('#display');
+let prevDisp = 0;
+let operator = 'equal';
+let overwrite = true;
 
-    //EVENT LISTENERS
+let init = () => {
 
-    //listen for number
-    $(".num").on('click', (event) => {
+  //EVENT LISTENERS
+  $(".num").click(printNumber);
+  $(".decimal").click(printDecimal);
+  $(".equal").click(equalOp);
+  $(".AC").click(allClear);
+  $(".sign").click(toggleSign);
+  $(".percent").click(makePercent)
+  $(".add").click(addOp)
+  $(".subtract").click(subtractOp)
+  $(".multiply").click(multiplyOp)
+  $(".divide").click(divideOp)
+  $(".equal").click(equalOp)
 
-      if (overwrite) {
-        clearDisp();
-        overwrite = false
-      }
-      //add digits
-      addDigit($(event.target).data('id'));
-      //parse string to float
-      $disp.data('id',parseFloat($disp.data('id')))
-      //print to display
-      displayData();
+}
 
+let printNumber = (e) => {
+  let digit = $(e.target).data('id')
 
-    })
-
-    //listen for decimal
-    $(".decimal").on('click', (event) => {
-
-      if (overwrite) {
-        clearDisp();
-        overwrite = false
-      }
-
-      //add decimal if there isn't one already
-      if ( $disp.text().indexOf('.') === -1 ) {
-        addDigit($(event.target).data('id'));
-        displayData();
-      }
-
-    })
-
-    //listen for equals
-    $(".equals").on('click', (event) => {
-
-      // evaluate 
-      let answer = evaluate();
-      $disp.text(answer);
-      oldDisp = answer;
-      overwrite = true;
-
-
-    }) 
-
-    //listen for addition
-    $(".add").on('click', (event) => {
-
-      let answer = evaluate();
-      oldDisp=$disp.text()
-      $disp.text(answer);
-      oldDisp = $disp.data('id');
-      operator = 1;
-      overwrite = true;
-    })
-
-    //listen for subtraction
-    $(".minus").on('click', (event) => {
-
-      let answer = evaluate();
-      oldDisp=$disp.text()
-      $disp.text(answer);
-      oldDisp = $disp.data('id');
-      operator = 2;
-      overwrite = true;
-    })
-
-    //listen for multiplication
-    $(".multiply").on('click', (event) => {
-
-      let answer = evaluate();
-      oldDisp=$disp.text()
-      $disp.text(answer);
-      oldDisp = $disp.data('id');
-      operator = 3;
-      overwrite = true;
-    })
-
-    //listen for division
-    $(".add").on('click', (event) => {
-
-      let answer = evaluate();
-      oldDisp=$disp.text()
-      $disp.text(answer);
-      oldDisp = $disp.data('id');
-      operator = 4;
-      overwrite = true;
-    })
-
-
-
-    //listen for clear
-    $(".AC").on('click', (event) => {
-      clearDisp();
-      oldDisp = 0;
-    })
-
-    //listen for negative
-    $(".neg").on('click', (event) => {
-      $disp.data('id',$disp.data('id')*-1);
-      displayData();
-    })
-
-    //listen for percent
-    $(".percent").on('click', (event) => {
-      $disp.data('id',$disp.data('id')*0.01);
-      displayData();
-    })
-
-
-    //FUNCTIONS
-
-    // adds a digit str to the display
-    let addDigit = (digit) => {
-      let dispStr = [$disp.data('id'), digit].join('');
-      $disp.data('id',dispStr);
-    } 
-
-    // put disp data into inner text
-    let displayData = () => {
-      $disp.text($disp.data('id'))
-    }
-
-    //clear display
-    let clearDisp = () => {
-      $disp.data('id','0')
-      $disp.text('0')
-    }
-
-    //evaluate
-    let evaluate = () => {
-      let currentDisp = $disp.data('id');
-
-      switch ( operator ) {
-        case 0:
-          console.log('case 0!')
-          break;
-        case 1:
-          return currentDisp + oldDisp;
-          break;
-        case 2:
-          return oldDisp - currentDisp;
-          break;
-        case 3:
-          return oldDisp * currentDisp;
-          break;
-        case 4:
-          return oldDisp / currentDisp;
-          break;
-
-      }
-
-
-
-      console.log(currentDisp)
-    }
-
+  if (overwrite) {
+    clearDisp();
+    overwrite = false
   }
 
-  $(documentReady)
+  addDigit(digit);
+  $disp.data('id',parseFloat($disp.data('id')))
+  displayData();
 
-})();
+}
+
+let printDecimal = () => {
+  if (overwrite) {
+    clearDisp();
+    overwrite = false
+  }
+  //add decimal if there isn't one already
+  if ( $disp.text().indexOf('.') === -1 ) {
+    addDigit('.');
+    displayData();
+  }
+
+}
+
+let addOp = () => {
+  evaluate();
+  operator = 'add';
+  $disp.data('id', 0);
+}
+
+let subtractOp = () => {
+  evaluate();
+  operator = 'subtract';
+  $disp.data('id', 0);
+}
+
+let multiplyOp = () => {
+  evaluate();
+  operator = 'multiply';
+  $disp.data('id', 1);
+}
+
+let divideOp = () => {
+  evaluate();
+  operator = 'divide';
+  $disp.data('id', 1);
+}
+
+let equalOp = () => {
+  evaluate(); 
+}
+
+let allClear = () => {
+  clearDisp();
+  prevDisp = 0;
+  operator = 'equal'
+}
+
+let toggleSign = () => {
+  $disp.data('id',$disp.data('id')*-1);
+  displayData();
+}
+
+let makePercent = ()=> {
+  $disp.data('id',$disp.data('id')*0.01);
+  displayData();
+}
+
+
+// //FUNCTIONS
+
+// adds a digit str to the display data
+let addDigit = (digit) => {
+  let dispStr = "" + $disp.data('id') + digit;
+  $disp.data('id',dispStr);
+} 
+
+// put disp data into inner text
+let displayData = () => {
+  $disp.text($disp.data('id'))
+}
+
+//clear display
+let clearDisp = () => {
+  $disp.data('id','0')
+  $disp.text('0')
+}
+
+//evaluate
+let evaluate = () => {
+  let currentDisp = $disp.data('id');
+
+  switch ( operator ) {
+    case 'equal':
+      prevDisp = currentDisp;
+      break;
+    case 'add':
+      prevDisp += currentDisp;
+      break;
+    case 'subtract':
+      prevDisp -=  currentDisp;
+      break;
+    case 'multiply':
+      prevDisp *= currentDisp;
+      break;
+    case 'divide':
+      prevDisp /= currentDisp;
+      break;
+  }
+
+  $disp.text(prevDisp)
+
+  overwrite = true;
+
+}
+
+
+
+$(document).ready(init)
 
 
 
